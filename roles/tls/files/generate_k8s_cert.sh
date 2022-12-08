4e5b789f46fc14a81bbdc9c07f84feb9d52a3376
niushaoshuai <you@example.com>
2022-12-08 17:08:23 +0800

    first commit

--- roles/tls/files/generate_k8s_cert.sh
+++ roles/tls/files/generate_k8s_cert.sh
@@ -2,6 +2,8 @@ cfssl gencert -initca ca-csr.json | cfssljson -bare ca -
 cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=kubernetes server-csr.json | cfssljson -bare server
 cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=kubernetes admin-csr.json | cfssljson -bare admin
 cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=kubernetes kube-proxy-csr.json | cfssljson -bare kube-proxy
+cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=kubernetes kube-controller-manager-csr.json | cfssljson -bare kube-controller-manager
+cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=kubernetes kube-scheduler-csr.json | cfssljson -bare kube-scheduler
 
 # 拷贝到使用证书的roles下
 root_dir=$(pwd |sed 's#ssl/k8s##')
